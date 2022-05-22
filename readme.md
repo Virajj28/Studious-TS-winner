3e7db9748c2160d082db23530068c3ca90e29f82
Virajj28 <viraj28j@gmail.com>
2022-05-22 15:59:27 +0530

    ts objs & aars

--- readme.md
+++ readme.md
@@ -5,3 +5,4 @@ Lesson2: tsc [filename.ts] , tsc [filename.ts] --w
 <br>
 Lesson3: Types basics for number,string,boolean
 <br>
+Lesson4: For objs n arrs,once type is done is fledged .Once we define Obj we can't add aditional properties to it.
--- sandbox.ts
+++ sandbox.ts
@@ -16,3 +16,37 @@ const circle = (diameter: number) => {
     return diameter*Math.PI;
 }
 console.log(circle(4.5));
+
+
+// Arrys & Objs
+
+let str = ['Hello', 'World'];
+str.push('!');
+// str.push(2); Not valid ts denial only strings
+
+let num = [1, 2, 3];
+num.push(4);
+// num.push('5'); Not valid ts denial only numbers
+
+let mixed = ['Hello', 1, 'World', 2, 3];
+mixed.push(4);
+mixed.push('TS Acceptance');
+
+
+let ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+  };
+  
+  ninja.age = 40;
+  ninja.name = 'ryu';
+  // ninja.age = '30';
+  // ninja.skills = ['fighting', 'sneaking']
+  
+  ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 40,
+    // skills: ['running'],
+  };
