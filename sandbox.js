d5bbb831b1863b0c7f9ff4759db7fed9e19ad982
Virajj28 <viraj28j@gmail.com>
2022-05-23 22:35:11 +0530

    arrs n objs js file

--- sandbox.js
+++ sandbox.js
@@ -10,3 +10,27 @@ var circle = function (diameter) {
     return diameter * Math.PI;
 };
 console.log(circle(4.5));
+// Arrys & Objs
+var str = ['Hello', 'World'];
+str.push('!');
+// str.push(2); Not valid ts denial only strings
+var num = [1, 2, 3];
+num.push(4);
+// num.push('5'); Not valid ts denial only numbers
+var mixed = ['Hello', 1, 'World', 2, 3];
+mixed.push(4);
+mixed.push('TS Acceptance');
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking']
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 40
+};
